9e69a9dc63965a0f8081666c67191999565cd607
Rosie Morissette <rosie.morissette@polymtl.ca>
2021-03-29 15:55:01 -0400

    exo_2_non_complété

--- exercice.py
+++ exercice.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import pandas as pd
 
 # TODO: Définissez vos fonctions ici
 
+def read_csv_file(path_file):
+    pd.read_csv(path_file)
 
 
 if __name__ == '__main__':
